da4377fa785eb1bf20f70504c5b48fdca016de05
jliip51 <jliip51@gmail.com>
2017-06-12 02:02:26 -0400

    Update v2

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+var config = {
+  apiKey: "AIzaSyDtR_Y1KmpfXmXf2FtSi_gD95yp5nomOTM",
+  authDomain: "train-app-hw.firebaseapp.com",
+  databaseURL: "https://train-app-hw.firebaseio.com",
+  projectId: "train-app-hw",
+  storageBucket: "train-app-hw.appspot.com",
+  messagingSenderId: "27551719601"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var startTime = "";
+var frequency = 0;
+
+function currentTime() {
+  var current = moment().format('LT');
+  $("#currentTime").html(current);
+  setTimeout(currentTime, 1000);
+};
+
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  if ($("#train-name").val().trim() === "" ||
+    $("#destination").val().trim() === "" ||
+    $("#first-train").val().trim() === "" ||
+    $("#frequency").val().trim() === "") {
+
+    alert("Please fill in all details to add new train");
+
+  } else {
+
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    startTime = $("#first-train").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    $(".form-field").val("");
+
+    database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      frequency: frequency,
+      startTime: startTime,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+  }
+
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+  var startTimeConverted = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
+  var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+  var timeRemain = timeDiff % childSnapshot.val().frequency;
+  var minToArrival = childSnapshot.val().frequency - timeRemain;
+  var nextTrain = moment().add(minToArrival, "minutes");
+  var key = childSnapshot.key;
+
+  var newrow = $("<tr>");
+  newrow.append($("<td>" + childSnapshot.val().trainName + "</td>"));
+  newrow.append($("<td>" + childSnapshot.val().destination + "</td>"));
+  newrow.append($("<td class='text-center'>" + childSnapshot.val().frequency + "</td>"));
+  newrow.append($("<td class='text-center'>" + moment(nextTrain).format("LT") + "</td>"));
+  newrow.append($("<td class='text-center'>" + minToArrival + "</td>"));
+  newrow.append($("<td class='text-center'><button class='arrival btn btn-danger btn-xs' data-key='" + key + "'>X</button></td>"));
+
+  $("#train-table-rows").append(newrow);
+
+});
+
+currentTime();
+
+$(document).on("click", ".arrival", function(){
+  keyref = $(this).attr("data-key");
+  database.ref().child(keyref).remove();
+  window.location.reload();
+});
